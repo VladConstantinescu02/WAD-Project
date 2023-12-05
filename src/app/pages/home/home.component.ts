6319159fb5efd54ee49e0e78d526dd6a9972e0b9
vladconstantinescu02 <constantinescuvladvsc@gmail.com>
2023-12-05 14:51:44 +0200

    Added component and mada changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,15 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styles: [
-  ]
+  templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
-
+  cols = 3;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onColmunsCountChange(colsNum: number): void  {
+    this.cols = colsNum;
+  }
 }
