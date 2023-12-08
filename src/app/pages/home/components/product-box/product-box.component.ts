e3618799c3c7be414a9fa6fa2c430c0460a274e5
vladconstantinescu02 <constantinescuvladvsc@gmail.com>
2023-12-08 17:34:16 +0200

    Finished Homepage

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ProductBoxComponent implements OnInit {
 
   @Input() fullWidthMode = false;
+px: any;
   
   constructor() { }
 
