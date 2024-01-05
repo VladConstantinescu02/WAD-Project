160b041c24d7c007ebc98a392dc881e3bcab70e8
vladconstantinescu02 <constantinescuvladvsc@gmail.com>
2024-01-05 17:48:12 +0200

    Started Cart page implementation

---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Cart, CartItem } from 'src/app/models/cart.model';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html'
+})
+export class CartComponent implements OnInit {
+
+  cart: Cart = { items: [{
+    product: 'http://placekitten.com',
+    name: 'sneakers',
+    price: 150,
+    quantity: 1,
+    id: 1
+  }] };
+
+  dataScource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action'
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.dataScource = this.cart.items;
+  }
+
+}
